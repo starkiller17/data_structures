0c542a7d5404c0b5d370b3831309c5256404834d
starkiller17 <eduardo.said.mndz@gmail.com>
2022-02-22 15:55:45 -0600

    data_structures add: get min and max from an array

---
+++ array-min-max.js
@@ -0,0 +1,20 @@
+var nums = [2, 1, 629, 25, 3525];
+
+function getMinAndMax(nums) {
+  // todo
+  let min = nums[0];
+  let max = nums[0];
+
+  nums.forEach( (num) => {
+    if ( num < min ) {
+      min = num;
+    }
+    if (num > max) {
+      max = num;
+    }
+  });
+
+  return [min, max];
+}
+
+console.log(getMinAndMax(nums));
